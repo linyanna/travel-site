80dd27f95e545c901aa0b8191bd86619f21bdce6
Yanna Lin <linyanna@yahoo.com>
2023-02-04 00:23:05 -0500

    adding assets

---
+++ build/precache-manifest.0c2392c4d2394c5efa5635a1d5269f3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf7c487fc3cd78a44c958238ba5f8b3",
+    "url": "/travel-site/index.html"
+  },
+  {
+    "revision": "cd7f71250b1037b32f4d",
+    "url": "/travel-site/static/css/main.7a327801.chunk.css"
+  },
+  {
+    "revision": "647bcdb9dc203c09c4bc",
+    "url": "/travel-site/static/js/2.501d64e7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/travel-site/static/js/2.501d64e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd7f71250b1037b32f4d",
+    "url": "/travel-site/static/js/main.213477d1.chunk.js"
+  },
+  {
+    "revision": "30e48d7fa107121c47e0",
+    "url": "/travel-site/static/js/runtime-main.4a61df93.js"
+  }
+]);
